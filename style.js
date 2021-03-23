a38257728038484cf2883db2d9d90fe6f7d01d66
developerachem <78043170+developerachem@users.noreply.github.com>
2021-03-24 02:28:07 +0600

    Update style.js

--- style.js
+++ style.js
@@ -11,12 +11,30 @@ function myFunction() {
 
     if (pricingchk.checked == true) {
         mainDib.style.display = "block";
+    } else {
+        mainDib.style.display = "none";
     }
-    else {mainDib.style.display = "none";}
 
     if (pricingchk.checked == false) {
         mainDib2.style.display = "block";
+    } else {
+        mainDib2.style.display = "none";
     }
-    else {mainDib2.style.display = "none";}
-     
+
 }
+var btn = $('#topBtn');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '300');
+});
